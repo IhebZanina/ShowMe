d494930a6288ff9ca345e4217f2e45edcaea7c9d
iheb <iheb.zanina@sousse.r-iset.tn>
2022-06-01 16:01:22 +0100

    Service for Getting Popular Movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/550?api_key=a4384b0dd6f7a41f98e835f4446b23ac');
+  }
 }
