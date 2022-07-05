0f713a65f88989479444724b32be2d9285cfb283
iheb <iheb.zanina@sousse.r-iset.tn>
2022-07-05 14:00:14 +0100

    Movies Page Layout

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Movie } from '../../Models/movie';
+import { MoviesService } from '../../services/movies.service';
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
 export class MoviesComponent implements OnInit {
-
-  constructor() { }
+  movies: Movie[] = [];
+  constructor(private movieService: MoviesService) {}
 
   ngOnInit(): void {
+    this.movieService.getMovies('popular', 20).subscribe((movies) => {
+      this.movies = movies;
+    });
   }
-
 }
