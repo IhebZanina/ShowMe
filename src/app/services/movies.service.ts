ab28e8864fea0e30a4b43b90960a3520aa27876b
iheb <iheb.zanina@sousse.r-iset.tn>
2022-07-05 05:23:30 +0100

    Adjust the Services to Return Specific Number of Movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MovieDto } from '../Models/movie';
+import { of, switchMap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +10,11 @@ export class MoviesService {
   baseURL: String = 'https://api.themoviedb.org/3';
   apiKey: String = 'a4384b0dd6f7a41f98e835f4446b23ac';
 
-  getMovies(type: string = 'upcoming') {
-    return this.http.get<MovieDto>(`${this.baseURL}/movie/${type}?api_key=${this.apiKey}`);
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseURL}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
   }
 }
