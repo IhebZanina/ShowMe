f285787f71fc9a0a41719e6c00d3d6bbf2996405
iheb <iheb.zanina@sousse.r-iset.tn>
2022-07-14 02:13:39 +0100

    Route and Get Movies By Category

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Movie } from '../../Models/movie';
+import { take } from 'rxjs/operators';
 import { MoviesService } from '../../services/movies.service';
 @Component({
   selector: 'app-movies',
@@ -8,16 +10,24 @@ import { MoviesService } from '../../services/movies.service';
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
-  constructor(private movieService: MoviesService) {}
+  constructor(private movieService: MoviesService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getpagedMovie(1);
+    this.route.params.pipe(take(1)).subscribe(({ genreId }) => {
+      if (genreId) {
+        this.getMovieByGenre(genreId);
+      } else {
+        this.getpagedMovie(1);
+      }
+    });
   }
   getpagedMovie(pageNumber: number) {
     this.movieService.searchMovies(pageNumber).subscribe((movies) => {
       this.movies = movies;
     });
   }
+  getMovieByGenre(page: number) {}
+
   paginate(event: any) {
     this.getpagedMovie(event.page + 1);
   }
